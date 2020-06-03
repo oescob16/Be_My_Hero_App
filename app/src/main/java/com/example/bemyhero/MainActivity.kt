64e5684ecc565be31bf60f5994a7cecee606232b
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2020-06-03 05:16:27 -0600

    Implemented Toolbar functions in MainActivity

--- app/src/main/java/com/example/bemyhero/MainActivity.kt
+++ app/src/main/java/com/example/bemyhero/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.bemyhero
 
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.annotation.NonNull
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
@@ -13,21 +17,41 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var navigationView: NavigationView
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     private lateinit var postList: RecyclerView
+    private lateinit var mToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mToolbar = findViewById(R.id.main_page_toolbar)
+        setSupportActionBar(mToolbar)
+        supportActionBar?.title = "Home"
+
         drawerLayout = findViewById(R.id.drawable_layout)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this@MainActivity, drawerLayout, R.string.drawer_open,R.string.drawer_close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         navigationView = findViewById(R.id.navigation_view)
+        var navView: View = navigationView.inflateHeaderView(R.layout.navigation_header)
 
         navigationView.setNavigationItemSelectedListener( NavigationView.OnNavigationItemSelectedListener { menuItem ->
             userMenuSelector(menuItem)
             false
         })
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
     private fun userMenuSelector(item: MenuItem){
         when(item.itemId){
             R.id.nav_profile -> {
