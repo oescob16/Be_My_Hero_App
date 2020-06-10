48cbe33dff7a7337e6869b8766fb5a65993fe9ab
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2020-06-09 22:54:09 -0600

    Added an progress bar, added methods to add the post images into the firebase database and storage.

--- app/src/main/java/com/example/bemyhero/PostActivity.kt
+++ app/src/main/java/com/example/bemyhero/PostActivity.kt
@@ -1,13 +1,24 @@
 package com.example.bemyhero
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
+import android.widget.*
 import androidx.appcompat.widget.Toolbar
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
 
 class PostActivity : AppCompatActivity() {
 
@@ -17,10 +28,37 @@ class PostActivity : AppCompatActivity() {
     private lateinit var postDescription: EditText
 
     private val galleryPick = 1
+    private var imageUri: Uri? = null
+    private lateinit var description: String
+
+    private lateinit var userPostImageRef: StorageReference
+    private lateinit var userRef: DatabaseReference
+    private lateinit var postRef: DatabaseReference
+    private lateinit var mAuth: FirebaseAuth
+
+    private lateinit var saveCurrDateAndTime: String
+    private lateinit var postRandomName: String
+
+    private lateinit var progressBar: AlertDialog
+
+    private lateinit var currUserId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+
+        mAuth = FirebaseAuth.getInstance()
+        currUserId = mAuth.currentUser?.uid.toString()
+
+        postRandomName = getDate()
+        progressBar = ProgressBar()
+
+        userRef = FirebaseDatabase.getInstance().reference.child("Users")
+        postRef = FirebaseDatabase.getInstance().reference.child("Posts")
+            .child(currUserId + postRandomName)
+        userPostImageRef = FirebaseStorage.getInstance().reference
+
+        userRef.keepSynced(true)
 
         selectPostImage = findViewById(R.id.select_post_image)
         updatePostButton = findViewById(R.id.update_post_button)
@@ -35,6 +73,10 @@ class PostActivity : AppCompatActivity() {
         selectPostImage.setOnClickListener {
             openGallery()
         }
+
+        updatePostButton.setOnClickListener {
+            updatePostInfo()
+        }
     }
 
     private fun openGallery(){
@@ -42,6 +84,88 @@ class PostActivity : AppCompatActivity() {
         galleryIntent.action = Intent.ACTION_GET_CONTENT
         galleryIntent.type = "image/*"
         startActivityForResult(galleryIntent,galleryPick)
+    }
+
+    private fun updatePostInfo(){
+        description = postDescription.text.toString()
+
+        if(imageUri == null){
+            Toast.makeText(this@PostActivity,"Please select an image!",Toast.LENGTH_SHORT).show()
+        }
+        else if(TextUtils.isEmpty(description)){
+            Toast.makeText(this@PostActivity,"Please provide details about your post!",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            storeImageToFirebaseStorage()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == galleryPick && resultCode == Activity.RESULT_OK && data != null){
+            imageUri = data.data!!
+            selectPostImage.setImageURI(imageUri)
+        }
+    }
+
+    private fun storeImageToFirebaseStorage() {
+        progressBar.show()
+
+        val filePath: StorageReference = userPostImageRef.child("Post Images")
+            .child(imageUri?.lastPathSegment + postRandomName + ".jpg")
+
+        imageUri?.let {
+            filePath.putFile(it).addOnSuccessListener(this) {Object: UploadTask.TaskSnapshot ->
+                filePath.downloadUrl.addOnSuccessListener(this) { uri: Uri ->
+
+                    val downloadUrl = uri.toString()
+
+                    postRef.child("image").setValue(downloadUrl).addOnCompleteListener { task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(this@PostActivity,"Profile image stored successfully to Firebase Storage!",Toast.LENGTH_SHORT).show()
+                            savingPostInfoToDatabase()
+                        }
+                        else {
+                            val message: String? = task.exception?.message
+                            Toast.makeText(this@PostActivity,"Error occurred: $message",Toast.LENGTH_SHORT).show()
+                            progressBar.dismiss()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun savingPostInfoToDatabase(){
+        userRef.child(currUserId).addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if(dataSnapshot.exists()){
+                    val userFullName: String = dataSnapshot.child("fullname").getValue().toString()
+                    val userProfileImage: String = dataSnapshot.child("profileimage").getValue().toString()
+
+                    val postMap = HashMap<String,String>()
+                    postMap.put("uid",currUserId)
+                    postMap.put("date&time",saveCurrDateAndTime)
+                    postMap.put("description",description)
+                    postMap.put("profileimage",userProfileImage)
+                    postMap.put("fullname",userFullName)
+
+                    postRef.updateChildren(postMap as Map<String, Any>).addOnCompleteListener { task ->
+                        if(task.isSuccessful){
+                            sendUserToMainActivity()
+                            Toast.makeText(this@PostActivity,"New post stored successfully to Firebase Database!",Toast.LENGTH_LONG).show()
+                            progressBar.dismiss()
+                        }
+                        else {
+                            Toast.makeText(this@PostActivity,"Error occurred while storing your post!",Toast.LENGTH_LONG).show()
+                            progressBar.dismiss()
+                        }
+                    }
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -55,6 +179,20 @@ class PostActivity : AppCompatActivity() {
     private fun sendUserToMainActivity(){
         val mainIntent: Intent = Intent(this@PostActivity,MainActivity::class.java)
         startActivity(mainIntent)
+    }
+
+    private fun ProgressBar(): AlertDialog {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this@PostActivity)
+        builder.setCancelable(false)
+        builder.setView(R.layout.loading_dialog)
+        return builder.create()
+    }
+
+    private fun getDate(): String{
+        val calendar: Calendar = Calendar.getInstance()
+        val currDateAndTime = SimpleDateFormat("dd-MM-yyyy-HH:mm:ss")
+        saveCurrDateAndTime = currDateAndTime.format(calendar.time)
+        return saveCurrDateAndTime
     }
 
 }
