5c2907dd5607d255a4fe6a3a1196cf05f78fc196
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2020-06-10 22:07:52 -0600

    Initial commit.

---
+++ app/src/main/java/com/example/bemyhero/Posts.kt
@@ -0,0 +1,10 @@
+package com.example.bemyhero
+
+class Posts {
+    var dateAndTime: String? = null
+    var description: String? = null
+    var fullname: String? = null
+    var image: String? = null
+    var profileimage: String? = null
+    var uid: String? = null
+}
