ee85732a7e82e040984a93d02468b3ae1cf29b28
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2020-06-11 23:17:03 -0600

    Added GoogleSignInClient, progressBar, and sendUserTo...Activity methods.

--- app/src/main/java/com/example/bemyhero/LoginActivity.kt
+++ app/src/main/java/com/example/bemyhero/LoginActivity.kt
@@ -5,17 +5,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.*
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.*
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.GoogleApi
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlin.math.sign
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var loginButton: Button
+    private lateinit var googleButton: ImageView
+
     private lateinit var userEmail: EditText
     private lateinit var userPassword: EditText
     private lateinit var newAccountLink: TextView
+
     private lateinit var mAuth: FirebaseAuth
+
+    private lateinit var progressBar: AlertDialog
+
+    private val RC_SIGN_IN: Int = 1
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private var TAG: String = "LoginActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +39,14 @@ class LoginActivity : AppCompatActivity() {
 
         mAuth = FirebaseAuth.getInstance()
 
+        progressBar = ProgressBar()
+
         newAccountLink = findViewById(R.id.register_account_link)
         userEmail = findViewById(R.id.login_email)
         userPassword = findViewById(R.id.login_password)
         loginButton = findViewById(R.id.login_button)
+
+        googleButton = findViewById(R.id.google_button)
 
         newAccountLink.setOnClickListener {
             SendUserToRegisterActivity()
@@ -35,19 +55,76 @@ class LoginActivity : AppCompatActivity() {
         loginButton.setOnClickListener {
             AllowUserToLogin()
         }
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        googleButton.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        googleSignInClient.signOut()
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+
+            progressBar.setTitle("Google Sign In")
+            progressBar.show()
+
+            val result: GoogleSignInResult? = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result != null) {
+                if(result.isSuccess){
+                    val account: GoogleSignInAccount? = result.signInAccount
+                    if (account != null) {
+                        firebaseAuthWithGoogle(account)
+                    }//.toString())
+                    Toast.makeText(this@LoginActivity,"Please wait, while we are getting your information!",Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    Toast.makeText(this@LoginActivity,"Sorry, but we are not able to get your information!",Toast.LENGTH_SHORT).show()
+                    progressBar.dismiss()
+                }
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount){
+        val credential = GoogleAuthProvider.getCredential(acct.idToken,null)
+
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithCredential:success")
+                    sendUserToMainActivity()
+                    progressBar.dismiss()
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    val message: String = task.exception.toString()
+                    sendUserToLoginActivity()
+                    Toast.makeText(this@LoginActivity,"Error occurred: $message.\nPlease, try again!",Toast.LENGTH_SHORT).show()
+                    progressBar.dismiss()
+                }
+            }
     }
 
     override fun onStart() {
         super.onStart()
         val currentUser: FirebaseUser? =  mAuth.currentUser
         if(currentUser != null){
-            SendUserToMainActivity()
+            sendUserToMainActivity()
         }
-    }
-
-    private fun SendUserToRegisterActivity(){
-        val registerIntent: Intent = Intent(this@LoginActivity,RegisterActivity::class.java)
-        startActivity(registerIntent)
     }
 
     private fun AllowUserToLogin(){
@@ -55,7 +132,7 @@ class LoginActivity : AppCompatActivity() {
         val email: String = userEmail.text.toString()
         val password: String = userPassword.text.toString()
 
-        val progressBar: AlertDialog = ProgressBar()
+        progressBar.setTitle("Manual Sign In")
         progressBar.show()
 
         if(TextUtils.isEmpty(email)){
@@ -68,7 +145,7 @@ class LoginActivity : AppCompatActivity() {
             mAuth.signInWithEmailAndPassword(email,password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful){
-                        SendUserToMainActivity()
+                        sendUserToMainActivity()
                         Toast.makeText(this@LoginActivity,"You have successfully logged in!",Toast.LENGTH_SHORT).show()
                         progressBar.dismiss()
                     }
@@ -81,10 +158,22 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun SendUserToMainActivity(){
+    private fun sendUserToMainActivity(){
         val mainIntent: Intent = Intent(this@LoginActivity,MainActivity::class.java)
         mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(mainIntent)
+        finish()
+    }
+
+    private fun SendUserToRegisterActivity(){
+        val registerIntent: Intent = Intent(this@LoginActivity,RegisterActivity::class.java)
+        startActivity(registerIntent)
+    }
+
+    private fun sendUserToLoginActivity(){
+        val loginIntent: Intent = Intent(this@LoginActivity,LoginActivity::class.java)
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(loginIntent)
         finish()
     }
 
