d0e90c27dc1f24ac72e351611047a4d4f5d87050
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2020-06-12 23:18:13 -0600

    Added two clickListeners, one onActivityResult, and two methods which helps the user to upload his information to the firebase storage and database

--- app/src/main/java/com/example/bemyhero/SettingsActivity.kt
+++ app/src/main/java/com/example/bemyhero/SettingsActivity.kt
@@ -1,14 +1,25 @@
 package com.example.bemyhero
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.activity_settings.*
 
@@ -27,16 +38,23 @@ class SettingsActivity : AppCompatActivity() {
 
     private lateinit var settingsRef: DatabaseReference
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var userProfileImageRef: StorageReference
 
     private lateinit var currUserId: String
+
+    private val galleryPick = 1
+    private lateinit var progressBar: AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        progressBar = ProgressBar()
+
         mAuth = FirebaseAuth.getInstance()
         currUserId = mAuth.currentUser?.uid.toString()
         settingsRef = FirebaseDatabase.getInstance().reference.child("Users").child(currUserId)
+        userProfileImageRef = FirebaseStorage.getInstance().reference.child("Profile Images")
 
         mToolbar = findViewById(R.id.settings_toolbar)
         setSupportActionBar(mToolbar)
@@ -85,5 +103,149 @@ class SettingsActivity : AppCompatActivity() {
 
         })
 
+        updateButton.setOnClickListener {
+            updateInfo()
+        }
+
+        userProfileImage.setOnClickListener {
+            val galleryIntent = Intent()
+            galleryIntent.action = Intent.ACTION_GET_CONTENT
+            galleryIntent.type = "image/*"
+            startActivityForResult(galleryIntent,galleryPick)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == galleryPick && data != null){
+            val imageUri: Uri? = data.data
+
+            CropImage.activity(imageUri)
+                .setGuidelines(CropImageView.Guidelines.ON_TOUCH)
+                .setAspectRatio(1, 1)
+                .start(this)
+        }
+
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && data != null){
+            val result: CropImage.ActivityResult = CropImage.getActivityResult(data)
+
+            progressBar.setTitle("Updating profile image")
+            progressBar.show()
+
+            if(resultCode == Activity.RESULT_OK){
+                val resultUri: Uri = result.uri
+                val filePath: StorageReference = userProfileImageRef.child(currUserId+".jpg")
+
+                filePath.putFile(resultUri).addOnSuccessListener(this) {
+                    filePath.downloadUrl.addOnSuccessListener(this) { uri: Uri ->
+
+                        Toast.makeText(this@SettingsActivity,"Profile image stored successfully to Firebase Storage!",Toast.LENGTH_SHORT).show()
+                        val downloadUrl: String = uri.toString()
+
+                        settingsRef.child("profileimage").setValue(downloadUrl).addOnCompleteListener { task ->
+                            if(task.isSuccessful){
+                                val selfIntent = Intent(this@SettingsActivity,SettingsActivity::class.java)
+                                selfIntent.flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT // new?
+                                startActivity(selfIntent)
+
+                                Toast.makeText(this@SettingsActivity,"Profile image stored successfully to Firebase Database!",Toast.LENGTH_SHORT).show()
+                                progressBar.dismiss()
+                            } else {
+                                val message: String? = task.exception?.message
+                                Toast.makeText(this@SettingsActivity,"Error Occurred: $message",Toast.LENGTH_SHORT).show()
+                                progressBar.dismiss()
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                Toast.makeText(this@SettingsActivity,"Error occurred: Image can't be cropped.\nTry again!",Toast.LENGTH_SHORT).show()
+                progressBar.dismiss()
+            }
+        }
+        if(data == null){
+            val selfIntent = Intent(this@SettingsActivity,SetupActivity::class.java)
+            selfIntent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+            startActivity(selfIntent)
+        }
+    }
+
+    private fun updateInfo(){
+        val newUsername: String = userName.text.toString()
+        val newDateOfBirth: String = userDateOfBirth.text.toString()
+        val newUserStatus: String = userStatus.text.toString()
+        val newRelationshipStatus: String = userRelationshipStatus.text.toString()
+        val newCountry: String = userCountry.text.toString()
+        val newFullName: String = userFullName.text.toString()
+        val newGender: String = userGender.text.toString()
+
+        if(TextUtils.isEmpty(newUserStatus)){
+            Toast.makeText(this@SettingsActivity,"Please enter your status!",Toast.LENGTH_SHORT).show()
+        }
+        else if(TextUtils.isEmpty(newFullName)){
+            Toast.makeText(this@SettingsActivity,"Please enter your full name!",Toast.LENGTH_SHORT).show()
+        }
+        else if(TextUtils.isEmpty(newUsername)){
+            Toast.makeText(this@SettingsActivity,"Please enter your username!",Toast.LENGTH_SHORT).show()
+        }
+        else if(TextUtils.isEmpty(newCountry)){
+            Toast.makeText(this@SettingsActivity,"Please enter your country!",Toast.LENGTH_SHORT).show()
+        }
+        else if(TextUtils.isEmpty(newDateOfBirth)){
+            Toast.makeText(this@SettingsActivity,"Please enter your date of birth!",Toast.LENGTH_SHORT).show()
+        }
+        else if(TextUtils.isEmpty(newGender)){
+            Toast.makeText(this@SettingsActivity,"Please enter your gender!",Toast.LENGTH_SHORT).show()
+        }
+        else if(TextUtils.isEmpty(newRelationshipStatus)){
+            Toast.makeText(this@SettingsActivity,"Please enter your relationship status!",Toast.LENGTH_SHORT).show()
+        }
+        else {
+            progressBar.setTitle("Saving new information")
+            progressBar.show()
+            updateAccount(newUserStatus,newFullName,newUsername,newCountry,newDateOfBirth,newGender,newRelationshipStatus)
+        }
+
+    }
+
+    private fun updateAccount(status: String, fullname: String, username: String, country: String, dob: String, gender: String, relationStatus: String) {
+        val userMap = HashMap<String,String>()
+
+        userMap.put("username",username)
+        userMap.put("status",status)
+        userMap.put("fullname",fullname)
+        userMap.put("country",country)
+        userMap.put("dateofbirth",dob)
+        userMap.put("gender",gender)
+        userMap.put("relationshipstatus",relationStatus)
+
+        settingsRef.updateChildren(userMap as Map<String, Any>).addOnCompleteListener(this) { task ->
+            if(task.isSuccessful){
+                sendUserToMainActivity()
+                Toast.makeText(this@SettingsActivity,"Your account has been updated successfully!",Toast.LENGTH_SHORT).show()
+                progressBar.dismiss()
+            }
+            else {
+                val message: String = task.exception?.message.toString()
+                Toast.makeText(this@SettingsActivity,"Error occurred: $message.\nPlease try again!",Toast.LENGTH_SHORT).show()
+                progressBar.dismiss()
+            }
+        }
+    }
+
+    private fun sendUserToMainActivity(){
+        val mainIntent: Intent = Intent(this@SettingsActivity,MainActivity::class.java)
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(mainIntent)
+        finish()
+    }
+
+    private fun ProgressBar(): AlertDialog {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this@SettingsActivity)
+        builder.setCancelable(false)
+        builder.setView(R.layout.loading_dialog)
+        return builder.create()
     }
 }
