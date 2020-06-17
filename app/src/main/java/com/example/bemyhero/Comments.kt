511f263c31dd8a3d566234a7f8d5f53bfd7ad1f0
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2020-06-16 22:51:41 -0600

    Initial commit. Module class.

---
+++ app/src/main/java/com/example/bemyhero/Comments.kt
@@ -0,0 +1,9 @@
+package com.example.bemyhero
+
+class Comments {
+    val comment: String? = null
+    val dateAndTime: String? = null
+    val fullname: String? = null
+    val uid: String? = null
+    val profileimage: String? = null
+}
