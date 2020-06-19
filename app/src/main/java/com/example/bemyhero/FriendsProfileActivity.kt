25eebe322b41f8bf0ab7f4c7f49a1d396d118903
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2020-06-18 22:35:27 -0600

    Added an onClickListener and button to send a friend request to any person you search. Change the text from "Send Friend Request" to "Cancel Friend Request". Added validations.

--- app/src/main/java/com/example/bemyhero/FriendsProfileActivity.kt
+++ app/src/main/java/com/example/bemyhero/FriendsProfileActivity.kt
@@ -1,11 +1,13 @@
 package com.example.bemyhero
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.firebase.auth.FirebaseAuth
@@ -22,14 +24,16 @@ class FriendsProfileActivity : AppCompatActivity() {
     private lateinit var profileCountry: TextView
     private lateinit var profileRelationship: TextView
     private lateinit var profileImage: CircleImageView
-    private lateinit var acceptRequestButton: Button
-    private lateinit var deleteRequestButton: Button
+    private lateinit var sendRequestButton: Button
+    private lateinit var declineRequestButton: Button
 
-    private lateinit var profileRef: DatabaseReference
+    private lateinit var friendsRequestRef: DatabaseReference
     private lateinit var friendsProfileRef: DatabaseReference
     private lateinit var mAuth: FirebaseAuth
     private lateinit var senderUserId: String
     private lateinit var receiverUserId: String
+
+    private lateinit var CURR_FRIENDS_STATE: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +43,42 @@ class FriendsProfileActivity : AppCompatActivity() {
         initializeActivityFields()
         displayFriendsProfile()
 
+        declineRequestButton.visibility = View.INVISIBLE
+        declineRequestButton.isEnabled = false
+
+        if(senderUserId != receiverUserId){
+            sendRequestButton.setOnClickListener {
+                sendRequestButton.isEnabled = false
+
+                if(CURR_FRIENDS_STATE == "not_friends"){
+                    sendFriendRequest()
+                }
+            }
+        }
+        else {
+            // In a future create a method to send the user to the profile activity
+            declineRequestButton.visibility = View.INVISIBLE
+            sendRequestButton.visibility = View.INVISIBLE
+        }
+
+    }
+
+    private fun sendFriendRequest(){
+        friendsRequestRef.child(senderUserId).child(receiverUserId)
+            .child("request_type").setValue("Sent")
+            .addOnCompleteListener(this) {task ->
+                if(task.isSuccessful){
+                    friendsRequestRef.child(receiverUserId).child(senderUserId)
+                        .child("request_type").setValue("Received")
+                        .addOnCompleteListener(this) { task ->
+                            if(task.isSuccessful){
+                                sendRequestButton.isEnabled = true
+                                CURR_FRIENDS_STATE = "request_sent"
+                                sendRequestButton.setText("Cancel Friend Request")
+                            }
+                        }
+                }
+            }
     }
 
     private fun displayFriendsProfile() {
@@ -69,6 +109,26 @@ class FriendsProfileActivity : AppCompatActivity() {
                     profileCountry.text = "Country: $countryData"
                     profileName.text = fullNameData
                     profileGender.text = "Gender: $genderData"
+
+                    statesOfRequestsButtons()
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+    }
+
+    private fun statesOfRequestsButtons(){
+        friendsRequestRef.child(senderUserId).addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if(dataSnapshot.hasChild(receiverUserId)){
+                    val requestType: String = dataSnapshot.child(receiverUserId).child("request_type").value.toString()
+                    if (requestType == "Sent"){
+                        CURR_FRIENDS_STATE = "request_sent"
+                        sendRequestButton.text = "Cancel Friend Request"
+
+                        declineRequestButton.visibility = View.INVISIBLE
+                        declineRequestButton.isEnabled = false
+                    }
                 }
             }
             override fun onCancelled(databaseError: DatabaseError) {}
@@ -78,7 +138,9 @@ class FriendsProfileActivity : AppCompatActivity() {
     private fun initializeFirebaseFields(){
         mAuth = FirebaseAuth.getInstance()
         receiverUserId = intent.extras?.get("FRIEND_ID").toString()
+        senderUserId = mAuth.currentUser?.uid.toString()
         friendsProfileRef = FirebaseDatabase.getInstance().reference.child("Users").child(receiverUserId)
+        friendsRequestRef = FirebaseDatabase.getInstance().reference.child("Friends_Requests")
     }
 
     private fun initializeActivityFields(){
@@ -90,7 +152,10 @@ class FriendsProfileActivity : AppCompatActivity() {
         profileRelationship = findViewById(R.id.friend_profile_relationship)
         profileStatus = findViewById(R.id.friend_profile_status)
         profileUsername = findViewById(R.id.friend_profile_user_name)
-        acceptRequestButton = findViewById(R.id.accept_friend_request_button)
-        deleteRequestButton = findViewById(R.id.decline_friend_request_button)
+
+        sendRequestButton = findViewById(R.id.send_friend_request_button)
+        declineRequestButton = findViewById(R.id.decline_friend_request_button)
+
+        CURR_FRIENDS_STATE = "not_friends"
     }
 }
