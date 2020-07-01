ae44ae121260c310d90b8f7da9d70790f9f9d9ae
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2020-07-01 00:48:18 -0600

    Initial commit. Module class.

---
+++ app/src/main/java/com/example/bemyhero/Messages.kt
@@ -0,0 +1,9 @@
+package com.example.bemyhero
+
+class Messages {
+    val date: String? = null
+    val from: String? = null
+    val message: String? = null
+    val time: String? = null
+    val type: String? = null
+}
