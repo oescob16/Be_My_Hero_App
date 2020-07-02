d9373ab2085e4d1d688a312fa7aa2e55fb0f1818
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2020-07-01 23:14:21 -0600

    Added more methods to make the class look more readable. Added two buttons to display the No. of posts and friends a user has.

--- app/src/main/java/com/example/bemyhero/ProfileActivity.kt
+++ app/src/main/java/com/example/bemyhero/ProfileActivity.kt
@@ -1,8 +1,12 @@
 package com.example.bemyhero
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -26,22 +30,57 @@ class ProfileActivity : AppCompatActivity() {
 
     private lateinit var currUserId: String
 
+    private lateinit var displayFriends: Button
+    private lateinit var displayPosts: Button
+    private lateinit var friendsRef: DatabaseReference
+    private var numFriends: Int = 0
+
+    private lateinit var postsRef: DatabaseReference
+    private var numPosts: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        mAuth = FirebaseAuth.getInstance()
-        currUserId = mAuth.currentUser?.uid.toString()
-        profileRef = FirebaseDatabase.getInstance().reference.child("Users").child(currUserId)
+        initializeFirebaseFields()
+        initializeActivityFields()
 
-        profileBirthday = findViewById(R.id.profile_birthday)
-        profileCountry = findViewById(R.id.profile_country)
-        profileGender = findViewById(R.id.profile_gender)
-        profileImage = findViewById(R.id.profile_image)
-        profileName = findViewById(R.id.profile_full_name)
-        profileRelationship = findViewById(R.id.profile_relationship)
-        profileStatus = findViewById(R.id.profile_status)
-        profileUsername = findViewById(R.id.profile_user_name)
+        displayFriends.setOnClickListener {
+            sendUserToFriendsActivity()
+        }
+
+        displayPosts.setOnClickListener {
+            sendUserToUserPostsActivity()
+        }
+
+        friendsRef.child(currUserId).addValueEventListener(object: ValueEventListener {
+            @SuppressLint("SetTextI18n")
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if(dataSnapshot.exists()){
+                    numFriends = dataSnapshot.childrenCount.toInt()
+                    displayFriends.text = "$numFriends Friends"
+                }
+                else {
+                    displayFriends.text = "0 Friends"
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+
+        postsRef.orderByChild("uid")
+            .startAt(currUserId).endAt(currUserId + "\uf8ff")
+            .addValueEventListener(object: ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        numPosts = dataSnapshot.childrenCount.toInt()
+                        displayPosts.text = "$numPosts Posts"
+                    }
+                    else {
+                        displayPosts.text = "0 Posts"
+                    }
+                }
+                override fun onCancelled(databaseError: DatabaseError) {}
+            })
 
         profileRef.addValueEventListener(object: ValueEventListener {
             @SuppressLint("SetTextI18n")
@@ -74,5 +113,37 @@ class ProfileActivity : AppCompatActivity() {
             }
             override fun onCancelled(databaseError: DatabaseError) {}
         })
+    }
+
+    private fun initializeFirebaseFields(){
+        mAuth = FirebaseAuth.getInstance()
+        currUserId = mAuth.currentUser?.uid.toString()
+        profileRef = FirebaseDatabase.getInstance().reference.child("Users").child(currUserId)
+        friendsRef = FirebaseDatabase.getInstance().reference.child("Friends")
+        postsRef = FirebaseDatabase.getInstance().reference.child("Posts")
+    }
+
+    private fun initializeActivityFields(){
+        profileBirthday = findViewById(R.id.profile_birthday)
+        profileCountry = findViewById(R.id.profile_country)
+        profileGender = findViewById(R.id.profile_gender)
+        profileImage = findViewById(R.id.profile_image)
+        profileName = findViewById(R.id.profile_full_name)
+        profileRelationship = findViewById(R.id.profile_relationship)
+        profileStatus = findViewById(R.id.profile_status)
+        profileUsername = findViewById(R.id.profile_user_name)
+
+        displayFriends = findViewById(R.id.profile_friends)
+        displayPosts = findViewById(R.id.profile_posts)
+    }
+
+    private fun sendUserToFriendsActivity(){
+        val friendsIntent = Intent(this@ProfileActivity,FriendsActivity::class.java)
+        startActivity(friendsIntent)
+    }
+
+    private fun sendUserToUserPostsActivity(){
+        val userPostIntent = Intent(this@ProfileActivity,UserPostsActivity::class.java)
+        startActivity(userPostIntent)
     }
 }
