8c7c010d5131f32500e8bb203bbba43ea65bd895
Oswaldo Escobedo <oescobedo3@miners.utep.edu>
2020-07-15 00:05:09 -0600

    Added a userRef and a currUserId. Added a method which updates the user's online status. Added two methods which gets the current date and time.

--- app/src/main/java/com/example/bemyhero/LoginActivity.kt
+++ app/src/main/java/com/example/bemyhero/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bemyhero
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +10,14 @@ import android.util.Log
 import android.widget.*
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.*
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.GoogleApi
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import kotlin.math.sign
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
 
 class LoginActivity : AppCompatActivity() {
 
@@ -27,6 +30,8 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var forgottenPasswordLink: TextView
 
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var userRef: DatabaseReference
+    private lateinit var currUserId: String
 
     private lateinit var progressBar: AlertDialog
 
@@ -39,6 +44,7 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         mAuth = FirebaseAuth.getInstance()
+        userRef = FirebaseDatabase.getInstance().reference.child("Users")
 
         progressBar = ProgressBar()
 
@@ -60,8 +66,17 @@ class LoginActivity : AppCompatActivity() {
         }
 
         loginButton.setOnClickListener {
-            AllowUserToLogin()
+            allowUserToLogin()
         }
+
+        createGoogleRequest()
+
+        googleButton.setOnClickListener {
+            googleSignIn()
+        }
+    }
+
+    private fun createGoogleRequest(){
 
         // Configure Google Sign In
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -69,15 +84,11 @@ class LoginActivity : AppCompatActivity() {
             .requestEmail()
             .build()
 
+        // Build a GoogleSignInClient with the options specified by gso.
         googleSignInClient = GoogleSignIn.getClient(this,gso)
-
-        googleButton.setOnClickListener {
-            signIn()
-        }
     }
 
-    private fun signIn() {
-        googleSignInClient.signOut()
+    private fun googleSignIn() {
         val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
@@ -85,6 +96,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
 
             progressBar.setTitle("Google Sign In")
@@ -92,13 +104,15 @@ class LoginActivity : AppCompatActivity() {
 
             val result: GoogleSignInResult? = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             if (result != null) {
+                // Google Sign In was successful, authenticate with Firebase
                 if(result.isSuccess){
                     val account: GoogleSignInAccount? = result.signInAccount
                     if (account != null) {
                         firebaseAuthWithGoogle(account)
-                    }//.toString())
+                    }
                     Toast.makeText(this@LoginActivity,"Please wait, while we are getting your information!",Toast.LENGTH_SHORT).show()
                 }
+                // Google Sign In failed
                 else {
                     Toast.makeText(this@LoginActivity,"Sorry, but we are not able to get your information!",Toast.LENGTH_SHORT).show()
                     progressBar.dismiss()
@@ -112,12 +126,14 @@ class LoginActivity : AppCompatActivity() {
 
         mAuth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
+                // Sign in success
                 if (task.isSuccessful) {
-                    Log.d(TAG, "signInWithCredential:success")
+                    updateOnlineStatus("online")
                     sendUserToMainActivity()
                     progressBar.dismiss()
-                } else {
-                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                }
+                // If sign in fails, display a message to the user.
+                else {
                     val message: String = task.exception.toString()
                     sendUserToLoginActivity()
                     Toast.makeText(this@LoginActivity,"Error occurred: $message.\nPlease, try again!",Toast.LENGTH_SHORT).show()
@@ -134,7 +150,7 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun AllowUserToLogin(){
+    private fun allowUserToLogin(){
 
         val email: String = userEmail.text.toString()
         val password: String = userPassword.text.toString()
@@ -152,6 +168,7 @@ class LoginActivity : AppCompatActivity() {
             mAuth.signInWithEmailAndPassword(email,password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful){
+                        updateOnlineStatus("online")
                         sendUserToMainActivity()
                         Toast.makeText(this@LoginActivity,"You have successfully logged in!",Toast.LENGTH_SHORT).show()
                         progressBar.dismiss()
@@ -163,6 +180,38 @@ class LoginActivity : AppCompatActivity() {
                     }
                 }
         }
+    }
+
+    private fun updateOnlineStatus(status: String) {
+
+        currUserId = mAuth.currentUser?.uid.toString()
+
+        val currDate: String = getDate()
+        val currTime: String = getTime()
+
+        val currStateMap = HashMap<String, String>()
+        currStateMap["date"] = currDate
+        currStateMap["time"] = currTime
+        currStateMap["type"] = status
+
+        if(currUserId != "null"){
+            userRef.child(currUserId).child("OnlineStatus")
+                .updateChildren(currStateMap as Map<String, Any>)
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun getDate(): String{
+        val calendar: Calendar = Calendar.getInstance()
+        val currDate = SimpleDateFormat("dd/MM/yyyy")
+        return currDate.format(calendar.time)
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun getTime(): String {
+        val calendar: Calendar = Calendar.getInstance()
+        val currTime = SimpleDateFormat("hh:mm a")
+        return currTime.format(calendar.time)
     }
 
     private fun sendUserToMainActivity(){
